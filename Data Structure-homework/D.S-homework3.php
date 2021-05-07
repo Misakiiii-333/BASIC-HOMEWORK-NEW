42a25bd6a9013682aee7c7fae5c865dd66da7664
Misaki <82492781+Misakiiii-333@users.noreply.github.com>
2021-05-07 15:26:55 +0900

    Update D.S-homework3.php

--- Data Structure-homework/D.S-homework3.php
+++ Data Structure-homework/D.S-homework3.php
@@ -7,19 +7,15 @@ $sum = 0;
 $avg = 0;
 $cnt = count($cal);
 
-// Loop through 2-dimention array
 foreach ($cal as $row) {
-    // Loop through each row
+    
     foreach ($row as $elm) {
-        // add element so tum
         $sum += $elm;
-
-        // check min element
+        
         if ($min > $elm) {
             $min = $elm;
         }
-
-        // check max element
+        
         if ($max < $elm) {
             $max = $elm;
         }
