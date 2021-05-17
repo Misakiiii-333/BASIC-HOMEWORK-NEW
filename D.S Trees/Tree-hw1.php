21afaebfaef2a027e98862f5866affd10cd34cd8
Misaki <82492781+Misakiiii-333@users.noreply.github.com>
2021-05-17 11:54:02 +0900

    Data StructureTree

---
+++ D.S Trees/Tree-hw1.php
@@ -0,0 +1,254 @@
+<?php
+
+/**
+* A node of Binary Tree (BT)
+*/
+class Node {
+    /** @var int */
+    private $data;
+
+    /** @var Node left subtree */
+    private $left;
+
+    /** @var Node right subtree */
+    private $right;
+
+    public function __construct($data, $left = null, $right = null)
+    {
+        $this->data = $data;
+        $this->left = $left;
+        $this->right = $right;
+    }
+
+    /**
+    * get data
+    * @return int
+    */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+    * set data
+    * @param int $data
+    */
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+    * get left
+    * @return Node
+    */
+    public function getLeft()
+    {
+        return $this->left;
+    }
+
+    /**
+    * set left
+    * @param Node $left
+    */
+    public function setLeft($left)
+    {
+        $this->left = $left;
+    }
+
+    /**
+    * get right
+    * @return Node
+    */
+    public function getRight()
+    {
+        return $this->right;
+    }
+
+    /**
+    * set right
+    * @param Node $right
+    */
+    public function setRight($right)
+    {
+        $this->right = $right;
+    }
+}
+
+/**
+* Binary Tree Class
+*/
+class BinaryTree {
+    /** @var Node */ 
+    protected $root;
+
+    public function __construct($root = null)
+    {
+        $this->root = $root;
+    }
+
+	public function isEmpty() {
+        return $this->root === null;
+    }
+
+    /**
+    * get root
+    * @return Node
+    */
+    public function getRoot()
+    {
+        return $this->root;
+    }
+
+    /**
+    * set root
+    * @param Node $root
+    */
+    public function setRoot($root)
+    {
+        $this->root = $root;
+    }
+ 
+    public function traverse($method) {
+        switch($method) {
+        
+            case 'inorder':
+            $this->_inorder($this->root);
+            break;
+        
+            case 'postorder':
+            $this->_postorder($this->root);
+            break;
+        
+            case 'preorder':
+            $this->_preorder($this->root);
+            break;
+        
+            default:
+            break;
+        } 
+    } 
+        
+    private function _inorder($node) {
+        
+        if($node->getLeft()) {
+            $this->_inorder($node->getLeft()); 
+        } 
+        
+        echo $node->getData(). " ";
+        
+        if($node->getRight()) {
+            $this->_inorder($node->getRight()); 
+        } 
+    }
+    
+    private function _preorder($node) {
+        
+        echo $node->getData(). " ";
+        
+        if($node->getLeft()) {
+            $this->_preorder($node->getLeft()); 
+        } 
+
+        if($node->getRight()) {
+            $this->_preorder($node->getRight()); 
+        } 
+    }
+    
+    private function _postorder($node) {
+        
+        if($node->getLeft()) {
+            $this->_postorder($node->getLeft()); 
+        } 
+        
+        
+        if($node->getRight()) {
+            $this->_postorder($node->getRight()); 
+        } 
+        
+        echo $node->getData(). " ";
+    }
+}  
+
+   
+class SearchBinaryTree extends BinaryTree {
+    public function insert($data) {
+              
+		if($this->root == NULL) {
+
+		$this->root = new Node($data);
+
+		} else {
+
+			$current = $this->root;
+
+			while(true) {
+
+					if($data < $current->getData()) {
+					
+						if($current->getLeft()) {
+							$current = $current->getLeft();
+						} else {
+							$current->setLeft(new Node($data));
+							break; 
+						}
+
+					} else if($data > $current->getData()){
+
+						if($current->getRight()) {
+							$current = $current->getRight();
+						} else {
+							$current->setRight(new Node($data));
+							break; 
+						}
+
+					} else {
+						break;
+					}
+			} 
+		}
+    }
+
+    public function getMaxelement() {
+        $currentNode = $this->root;//現在いる地点
+        while(true){
+            if($currentNode->getRight() == null){
+                $maxdata = $currentNode->getData(); //getData(データの値)をかえす
+                break;
+            } else {
+                // $maxdata = $node->getRight()->getData();
+                $currentNode = $currentNode->getRight(); //右下のnodeに視点を移す
+            }
+            
+        }
+        echo $maxdata."\n";
+        return $maxdata;
+    }  
+    public function getMinimumelement() {
+        $currentNode = $this->root;//現在いる地点
+        while(true){
+            if($currentNode->getLeft() == null){
+                $mindata = $currentNode->getData(); //getData(データの値)をかえす
+                break;
+            } else { //データがある時
+                $currentNode = $currentNode->getLeft(); //左下のnodeに視点を移す                
+            }
+        }
+        echo $mindata."\n";
+        return $mindata;                 
+    }
+}  
+
+
+$arr = array(10, 5, 19, 1 ,6, 17);
+$tree = new SearchBinaryTree();
+for($i=0,$n=count($arr);$i < $n;$i++) {
+    $tree->insert($arr[$i]);
+}
+// echo "\nFind value 5 in tree:\n"; 
+// $tree->search(5);
+// echo "\nFind value 18 in tree:\n"; 
+// $tree->search(18);
+$tree->getMaxelement();
+$tree->getMinimumelement();
+?>
---
+++ D.S Trees/Tree-hw2.php
@@ -0,0 +1,149 @@
+<?php
+/**
+* A node of Binary Tree (BT)
+*/
+class Node {
+    /** @var int */
+    private $data;
+
+    /** @var Node left subtree */
+    private $left;
+
+    /** @var Node right subtree */
+    private $right;
+
+    public function __construct($data, $left = null, $right = null)
+    {
+        $this->data = $data;
+        $this->left = $left;
+        $this->right = $right;
+    }
+
+    /**
+    * get data
+    * @return int
+    */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+    * set data
+    * @param int $data
+    */
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+    * get left
+    * @return Node
+    */
+    public function getLeft()
+    {
+        return $this->left;
+    }
+
+    /**
+    * set left
+    * @param Node $left
+    */
+    public function setLeft($left)
+    {
+        $this->left = $left;
+    }
+
+    /**
+    * get right
+    * @return Node
+    */
+    public function getRight()
+    {
+        return $this->right;
+    }
+
+    /**
+    * set right
+    * @param Node $right
+    */
+    public function setRight($right)
+    {
+        $this->right = $right;
+    }
+}
+Class BinaryTree { //BinaryTree class
+    private $root;
+    private $arr;
+
+    public function __construct($root = null,$arr = array()) {
+        $this->root = $root;
+        $this->arr = $arr;
+    }
+
+    public function getRoot() {
+        return $this->root;
+    }
+    public function setRoot($root) {
+        $this->root = $root;
+    }
+
+    public function addNewData($newNumber) { //add new data
+        if ($this->root === null) { //root = null
+            $this->root = new Node($newNumber);
+            return;
+        }
+        for ($n = 0;$n < count($this->arr);$n++) { 
+            if($this->arr[$n] == null) {
+                $this->arr[$n] = new Node($newNumber);
+                break;
+            }
+        }
+        $i = 0;
+        while ($i < count($this->arr)) {
+            $current = $this->arr[$i];
+            if($current->getLeft() === null) { 
+                $current->setLeft(new Node($newNumber));
+                array_push($this->arr,$current->getLeft());
+               break;
+            } elseif($current->getRight() === null) { //if right nood = null
+                $current->setRight(new Node($newNumber)); //add new node 
+                array_push($this->arr,$current->getRight());
+                break;
+            } else {
+                $i++;
+            }
+        }
+    }
+    public function showTheTree() { //show tree
+        print_r($this->root);
+    }
+    
+    public function arrayPush($data) {
+        array_push($this->arr,$data);
+    }
+}
+        $leaf1 = new Node(7);
+        $leaf2 = new Node(15);
+        $leaf3 = new Node(8);
+        
+        $parent1 = new Node(11, $leaf1); //この下にleaf(12)を追加したい
+        $parent2 = new Node(9, $leaf2, $leaf3); 
+        $parent3 = new Node(10, $parent1,$parent2); 
+        $root = $parent3;
+        $bt = new BinaryTree($root);
+        
+        
+        $bt->arrayPush($leaf1);
+        $bt->arrayPush($leaf2);
+        $bt->arrayPush($leaf3);
+        $bt->arrayPush($parent1->getRight());
+        $bt->arrayPush($parent1);
+        $bt->arrayPush($parent2);
+        $bt->arrayPush($parent3);
+        
+        $bt->addNewdata(12); //add leaf(12)
+        $bt->showTheTree();
+        
+        ?>
