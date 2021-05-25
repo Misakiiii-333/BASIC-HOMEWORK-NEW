788d4902c910b6525605cc2f51c96f0d3e44c597
Misaki <82492781+Misakiiii-333@users.noreply.github.com>
2021-05-25 17:04:24 +0900

    Update homework1.php

--- Algorithm practice-new/homework1.php
+++ Algorithm practice-new/homework1.php
@@ -1,6 +1,6 @@
 <?php
 function findLargestSumPair($arr, $n) {
-
+    
     $max;
     $secondMax;
      
@@ -11,23 +11,23 @@ function findLargestSumPair($arr, $n) {
         $max = $arr[1];
         $secondMax = $arr[0];
     }
- 
-    for ( $i = 2; $i < $n; $i++) {
 
-        if ($arr[$i] > $max) {
+    for ( $i = 2; $i < $n; $i ++) {
+ 
+        if ($arr[$i] > $max)
+        {
             $secondMax = $max;
             $max = $arr[$i];
-        }else if ($arr[$i] >  $secondMax and $arr[$i] != $max) {
+        } else if ($arr[$i] > $secondMax and $arr[$i] != $max) {
             $secondMax = $arr[$i];
         }
-    return ($max + $secondMax);
     }
+    return ($max + $secondMax);
 }
  
     // Driver Code
     $arr = array(0, 2, 1, 9, 7);
     $n = count($arr);
-    echo "Largest pair Sum :" 
-    findLargestSumPair($arr, $n);
+    echo "Largest Pair Sum : ",findLargestSumPair($arr, $n);
  
 ?>
