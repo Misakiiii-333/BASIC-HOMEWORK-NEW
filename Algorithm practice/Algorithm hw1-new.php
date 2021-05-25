85e31dd68be07e10f4ff8dcaa1db5cbeab7f8cce
Misaki <82492781+Misakiiii-333@users.noreply.github.com>
2021-05-25 14:01:10 +0900

    Update Algorithm hw1-new.php

--- Algorithm practice/Algorithm hw1-new.php
+++ Algorithm practice/Algorithm hw1-new.php
@@ -1,20 +1,15 @@
 <?php
 
-function largest_pair($arr, $n)
-{
+function largest_pair($arr, $n) {
     $min = PHP_INT_MAX;
     $secondMin = PHP_INT_MAX;
-    for ($j = 0; $j < $n; $j++)
-    {
-        if ($arr[$j] > $max)
-        {
-            $secondMax = $max;
-            
+    for ($j = 0; $j < $n; $j++) {
+        if ($arr[$j] > $max) {
+            $secondMax = $max;            
             $max = $arr[$j];
+            
         } else if (($arr[$j] > $secondMax) && $arr[$j] != $max) {
- 
-            //secondMin update
-            $secondMax = $arr[$j];
+            $secondMax = $arr[$j]; //secondMin update
         }
     }
     // Return the sum of the minimum pair
