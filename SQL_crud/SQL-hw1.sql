4644747fc607ff6af8100027a4666cf3f0a4746f
Misaki <82492781+Misakiiii-333@users.noreply.github.com>
2021-05-27 13:39:57 +0900

    Update SQL-hw1.sql

--- SQL_crud/SQL-hw1.sql
+++ SQL_crud/SQL-hw1.sql
@@ -3,7 +3,9 @@ SELECT DISTINCT agents.*
 FROM agents,customer
 WHERE 
     agents.WORKING_AREA = 'LONDON'
+AND 
     customer.CUST_COUNTRY = 'UK'
+AND 
     agents.AGENT_CODE = customer.AGENT_CODE;
 
 ALTER TABLE new _agents ADD PRIMARY KEY(AGENT_CODE);
