c86ec0e28eb9752edbbff2287bd59398fec83920
Misaki <82492781+Misakiiii-333@users.noreply.github.com>
2021-05-27 13:08:32 +0900

    Update SQL-hw1.sql

--- SQL_crud/SQL-hw1.sql
+++ SQL_crud/SQL-hw1.sql
@@ -5,5 +5,5 @@ WHERE
     agents.WORKING_AREA = 'LONDON'
     customer.CUST_COUNTRY = 'UK'
     agents.AGENT_CODE = customer.AGENT_CODE;
-// made PRIMARY KEY   
+
 ALTER TABLE new _agents ADD PRIMARY KEY(AGENT_CODE);
