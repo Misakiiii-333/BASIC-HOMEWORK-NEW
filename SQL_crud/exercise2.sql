668eb5c5cf5f28a9d31971585426d2010e7f6574
Misaki <82492781+Misakiiii-333@users.noreply.github.com>
2021-05-27 14:13:34 +0900

    Update exercise2.sql

--- SQL_crud/exercise2.sql
+++ SQL_crud/exercise2.sql
@@ -1,14 +1,16 @@
 UPDATE agents
-SET COMMISSION = COMMISSION +.02
-WHERE AGENT_CODE IN (
-    SELECT agents.AGENT_CODE
-    FROM agents
-    WHERE(
-        SELECT 
-            COUNT(customer.AGENT_CODE)
-        FROM 
-            customer
-        WHERE 
-            customer.AGENT_CODE = agents.AGENT_CODE
-    ) >= 2
-);
+SET 
+    COMMISSION = COMMISSION +.02
+WHERE 
+    AGENT_CODE IN (
+        SELECT agents.AGENT_CODE
+        FROM agents
+        WHERE(
+            SELECT 
+                COUNT(customer.AGENT_CODE)
+            FROM 
+                customer
+            WHERE 
+                customer.AGENT_CODE = agents.AGENT_CODE
+        ) >= 2
+    );
