2e7e429377088ff59f70c8d4fbd0b4beb57ecfb0
Misaki <82492781+Misakiiii-333@users.noreply.github.com>
2021-05-27 13:17:05 +0900

    Create exercise2.sql

---
+++ SQL_crud/exercise2.sql
@@ -0,0 +1,15 @@
+UPDATE agents
+SET COMMISSION = COMMISSION +.02
+WHERE
+    AGENT_CODE IN (
+        SELECT agents.AGENT_CODE
+        FROM agents
+        WHERE(
+            SELECT 
+                COUNT(customer.AGENT_CODE)
+            FROM 
+                customer
+            WHERE 
+            customer.AGENT_CODE = agents.AGENT_CODE
+        ) >= 2
+        );
